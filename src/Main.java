27759fcf53b4a04b8965b7b8007dfcdc8446d822
Carinsay <3624089541@qq.com>
2025-09-02 14:32:14 +0800

    Task-0

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        Person p = new Person();
+        p.name = "Carinsay";
+        p.age = 19;
+        p.sayhello();
+        int a =  p.sum(7,8);
+        System.out.println(a);
+
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    String name;
+    int age;
+    String gender;
+
+
+    void sayhello() {
+        System.out.println("我叫"+ name + "今年" + age +"岁了");
+}
+    int sum(int a,int b)
+    {
+        return a+b;
+    }
+}
