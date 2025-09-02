412610729c97e3674ab843930b55e14a6b17bfa1
Carinsay <3624089541@qq.com>
2025-09-02 15:25:47 +0800

    Task-0

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 public class Main{
     public static void main(String[] args)
     {
-        Person p = new Person();
-        p.name = "Carinsay";
-        p.age = 1;
-        p.sayhello();
-        int a =  p.sum(7,8);
-        System.out.println(a);
+        System.out.println("hello world");
 
     }
 }
